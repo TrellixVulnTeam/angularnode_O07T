1414a0eecdf3d7d0f5109f327726bb4366617067
Gustavo Benedito <gustavo.benedito@ws-solution.com>
2022-01-12 23:52:59 -0300

    ajustes no frontend na criacao de categorias e dispositivos

--- front/src/app/categoria/categoria.component.ts
+++ front/src/app/categoria/categoria.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categoria.component.css']
 })
 export class CategoriaComponent implements OnInit {
-
+  categorias=[
+    ["1","nometeste"],
+    ["2","nometeste"],
+    ["3","nometeste"]];
   constructor() { }
 
   ngOnInit(): void {
